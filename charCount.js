99489fc2b850916da4f081d3e17a7f870f2907f2
Simon <simon@simonhamilton.ca>
2018-01-17 17:45:45 +0000

    final code for char count

--- charCount.js
+++ charCount.js
@@ -1,6 +1,22 @@
-function countLetters (string) {
-  string = string.split('').join('')
-  return string
+var letters = {};
+
+function countLetters(string) {
+  string =  string.replace(/\s/g,'').split('');
+for (var i = 0; i < string.length; i++) {
+
+  if (letters[string[i]]) {
+    letters[string[i]] += 1;
+  } else {
+    letters[string[i]] = 1;
+  }
+
+}
+  console.log(letters);
 }
 
-countLetters('lighthouse labs')
+countLetters('lighthouse in the house');
+
+
+
+// return to us all the unique characters that exist
+// in a string that is passed into the function.
